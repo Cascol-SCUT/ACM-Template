8abe9509e6853ef1b452ec0264baa514c7bb625f
root <root@LAPTOP-6Q0JN1C7.localdomain>
2021-01-26 15:47:14 +0800

    first commit

---
+++ project28.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define maxN 200005
+typedef long long ll;
+using namespace std;
+void solve()
+{
+    
+}
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T;cin>>T;
+    while(T--)
+    {
+        solve();   
+    }
+}
